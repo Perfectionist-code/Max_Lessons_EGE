0994eb4dbec0a90647f1cccf63850a9212fbcfc5
Сергей Анатольевич <skirdov-ser@yandex.ru>
2026-02-07 08:25:05 +0300

    Занятие №4 добавление некоторых методов решения в задачи 04 и 06

--- Lesson -4_Task_24_Linear_method-2026_02_06/04_Task_24.py
+++ Lesson -4_Task_24_Linear_method-2026_02_06/04_Task_24.py
@@ -1,3 +1,12 @@
+# метод 1
+# with open('04_24.txt') as f:
+#     s = f.readline()
+# while 'XX' in s or 'YY' in s or 'ZZ' in s:
+#     s = s.replace('XX', 'X X').replace('YY', 'Y Y').replace('ZZ', 'Z Z')
+# s = s.split()
+# print(len(max(s,key=len)))
+
+
 # метод 2
 # with open('04_24.txt') as f:
 #     s = f.readline()
@@ -13,3 +22,4 @@
 #         else:
 #             break
 # print(m)
+
--- Lesson -4_Task_24_Linear_method-2026_02_06/06_Task_24.py
+++ Lesson -4_Task_24_Linear_method-2026_02_06/06_Task_24.py
@@ -1,3 +1,10 @@
+# метод 1
+# with open('06_24.txt') as f:
+#     s = f.readline()
+# s = s.replace('ad', 'a d').replace('da', 'd a').split()
+# print(len(max(s,key=len)))
+
+# метод 2
 # with open('06_24.txt') as f:
 #     s = f.readline()
 # print((ls := len(s)))
@@ -13,14 +20,14 @@
 #             break
 # print(m)
 
-
+# метод 3
 from re import *
 
 with open('06_24.txt') as f:
     s = f.readline()
 print((ls := len(s)))
-
-reg = r'[^(da)(ad)]+'
+s = s.replace('ad', 'a#d').replace('da', 'd#a')
+reg = r'[^#]*'
 reg = fr'(?=({reg}))'
 m = max([x.group(1) for x in finditer(reg, s)], key=len)
 print(len(m), m)
