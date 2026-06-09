76d27593795874fd314f78aaf2bbc08f46a65ff2
Сергей Анатольевич <skirdov-ser@yandex.ru>
2026-06-09 17:09:29 +0300

    Занятие 2026_06_09 Задачи 15

--- Lesson -17_Task_15_сложные_ТИП_ЕГКР/02_1.py
+++ Lesson -17_Task_15_сложные_ТИП_ЕГКР/02_1.py
@@ -11,7 +11,7 @@ def f(x, y):
     return (49531739 != 14 * y + 15 * x) or (A < x) or (A < y)
 
 
-pairs = tuple((x, (49531739 - 15 * x) // 14) for x in range(1, 3302117) if 49531739 - 15 * x > 0 and (49531739 - 15 * x) % 14 == 0)
+pairs = set((x, (49531739 - 15 * x) // 14) for x in range(1, 3302117) if 49531739 - 15 * x > 0 and (49531739 - 15 * x) % 14 == 0)
 print(len(pairs))
 
 for A in range(2000000, 0, -1):
